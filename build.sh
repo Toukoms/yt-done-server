d912893f47ea8e348127b524553e228ecc9e9ff4
Toukoms <tokiniaina090605@gmail.com>
2025-03-19 08:57:35 +0300

    Update CI workflow and build script to include FFmpeg and yt-dlp installation

--- build.sh
+++ build.sh
@@ -1,2 +1,24 @@
-export PATH=$PATH:$HOME/bin/
-npm install
+#!/bin/bash
+
+# Create a bin directory
+mkdir -p bin
+
+# Download & extract FFmpeg
+wget -q https://github.com/BtbN/FFmpeg-Builds/releases/download/latest/ffmpeg-n7.1-latest-linux64-gpl-7.1.tar.xz
+tar -xf ffmpeg-n7.1-latest-linux64-gpl-7.1.tar.xz
+mv ffmpeg-n7.1-latest-linux64-gpl-7.1/bin/* bin/
+rm -rf ffmpeg-n7.1-latest-linux64-gpl-7.1*
+
+# Download yt-dlp
+wget -q https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp -O bin/yt-dlp
+chmod +x bin/*  # Make files executable
+
+# Show success message
+echo "✅ Installation complete! Binaries are in ./bin"
+ls -lh bin
+
+export PATH=$PWD/bin:$PATH
+yt-dlp --version
+echo "✅ exported"
+
+npm ci
